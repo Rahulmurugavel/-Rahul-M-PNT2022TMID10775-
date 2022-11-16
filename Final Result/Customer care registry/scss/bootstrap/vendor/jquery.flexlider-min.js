c1cfd2b2d059ea22e606a4d97d777092a3a86d5e
Rahulmurugavel <113689112+Rahulmurugavel@users.noreply.github.com>
2022-11-17 02:08:45 +0530

    Add files via upload

---
+++ Final Result/Customer care registry/scss/bootstrap/vendor/jquery.flexlider-min.js
@@ -0,0 +1,9 @@
+/*
+
+ * jQuery FlexSlider v2.6.0
+
+ * Copyright 2012 WooThemes
+
+ * Contributing Author: Tyler Smith
+
+ */!function($){var e=!0;$.flexslider=function(t,a){var n=$(t);n.vars=$.extend({},$.flexslider.defaults,a);var i=n.vars.namespace,s=window.navigator&&window.navigator.msPointerEnabled&&window.MSGesture,r=
